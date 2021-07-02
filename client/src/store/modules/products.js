b33cd4b9ff0f38f6fb94ca5152ef180fead7242d
Pontus Sidlo <poppe9898@hotmail.com>
2021-07-02 12:11:41 +0200

    fixed title and meta tags

--- client/src/store/modules/products.js
+++ client/src/store/modules/products.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://e-commerce-vue-school-project.herokuapp.com'
+const BASE_URL = "https://e-commerce-vue-school-project.herokuapp.com";
 
 export default {
   state: {
